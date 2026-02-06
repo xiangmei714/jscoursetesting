07ec28be062ca9fc467818e13e2d6390e803f120
xiangmei714 <lixiangmei2018@gmail.com>
2026-02-06 08:57:46 -0500

    control flow exercise 2

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,3 +46,8 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
